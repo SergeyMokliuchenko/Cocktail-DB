d95f1f25b8f7deb150becd73d74e85be1b0e83a7
Serhii Mokliuchenko <s.mokliuchenko@gmail.com>
2020-07-10 10:41:50 +0300

    Create project

---
+++ Cocktail DB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cocktail DB
+//
+//  Created by Serhii Mokliuchenko on 10.07.2020.
+//  Copyright © 2020 Serhii Mokliuchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
